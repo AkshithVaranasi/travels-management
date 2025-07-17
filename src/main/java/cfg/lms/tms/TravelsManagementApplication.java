69e1fd76e08300dc810ea704c59520717a38e7a2
Akshith510 <2100080161ai.ds@gmail.com>
2025-07-17 11:45:25 +0530

    initial commit

---
+++ src/main/java/cfg/lms/tms/TravelsManagementApplication.java
@@ -0,0 +1,13 @@
+package cfg.lms.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cfg/lms/tms/TravelsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cfg.lms.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
